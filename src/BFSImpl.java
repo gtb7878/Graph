fe6c368ffcc241dacb89da1b96880fb57e92aa61
gtb7878 <gtb7878@freenet.de>
2020-06-27 12:59:21 +0200

    implemented Graph (todo: transpose), BFS

--- src/BFSImpl.java
+++ src/BFSImpl.java
@@ -1,20 +1,73 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+
 public class BFSImpl implements BFS
 {
+    int[] dist;
+    int[] pred;
+
+
+    // siehe Folie 134.
     @Override
     public void search(Graph g, int s)
     {
+        if (g == null) return;
 
+
+        dist = new int[g.size()];
+        pred = new int[g.size()];
+
+        // 1
+        for (int i = 0; i < g.size(); i++)
+        {
+            dist[i] = INF;
+            pred[i] = NIL;
+        }
+        if (s < 0 || s >= g.size()) return;
+
+
+        // 2
+        dist[s] = 0;
+        pred[s] = NIL;
+
+        // 3
+        Queue<Integer> waitingQueue = new LinkedList<>();
+        waitingQueue.add(s);
+
+        // 4
+        int u, v;
+        while (!waitingQueue.isEmpty())
+        {
+            // 4.1
+            u = waitingQueue.poll();
+
+            // 4.2
+            for (int i = 0; i < g.deg(u); i++)
+            {
+                v = g.succ(u, i);
+                if (dist[v] == INF)
+                {
+                    // 4.2.1
+                    dist[v] = dist[u] + 1;
+                    pred[v] = u;
+
+                    // 4.2.2
+                    waitingQueue.add(v);
+                }
+            }
+        }
     }
 
     @Override
     public int dist(int v)
     {
-        return 0;
+        return dist[v];
     }
 
     @Override
     public int pred(int v)
     {
-        return 0;
+        return pred[v];
     }
 }
--- src/GraphImpl.java
+++ src/GraphImpl.java
@@ -1,26 +1,38 @@
 public class GraphImpl implements  Graph
 {
+    private int[][] amatrix;
+    public GraphImpl(int[][] m)
+    {
+        amatrix = m;
+    }
+
     @Override
     public int size()
     {
-        return 0;
+        return amatrix.length;
     }
 
     @Override
     public int deg(int v)
     {
-        return 0;
+        if (v < 0 || v >= size()) return -1;
+        return amatrix[v].length;
     }
 
     @Override
     public int succ(int v, int i)
     {
-        return 0;
+        if (v < 0 || v >= size()) return -1;
+        else
+        {
+            if (i < 0 || i >= deg(v)) return -1;
+            return amatrix[v][i];
+        }
     }
 
     @Override
     public Graph transpose()
     {
-        return null;
+        return null;  // TODO
     }
 }
